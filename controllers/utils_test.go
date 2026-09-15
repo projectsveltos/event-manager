549052eebad7c46a48c721f8ef8d19ac53979ff6
Akanksha Trehun <akankshatrehun@gmail.com>
2026-09-15 19:33:03 +0530

    Update cluster-api import path to match current main

--- controllers/utils_test.go
+++ controllers/utils_test.go
@@ -22,7 +22,7 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	apiextensionsv1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	clusterv1 "sigs.k8s.io/cluster-api/api/v1beta1"
+	clusterv1 "sigs.k8s.io/cluster-api/api/core/v1beta2"
 
 	"github.com/projectsveltos/event-manager/api/v1beta1"
 	"github.com/projectsveltos/event-manager/controllers"
